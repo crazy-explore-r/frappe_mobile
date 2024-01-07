9ed5d98da8b952071211656396369d1864a4e8c7
crazy-explore-r <developer.ragul@gmail.com>
2024-01-07 23:05:18 +0530

    cleanup

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URI = "http://10.10.0.67";
+export const BASE_URI = "https://erpnoveloffice.in";
 
 // must match the "scheme" property of the "app.json" file
 export const REDIRECT_URL_SCHEME = "dev.noveloffice.in"; 
 
 export const SECURE_AUTH_STATE_KEY = "AuthState";
-export const OAUTH_CLIENT_ID = "74787e06b1";
+export const OAUTH_CLIENT_ID = "e0ecbcc2fc";
